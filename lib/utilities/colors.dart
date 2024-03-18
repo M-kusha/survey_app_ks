82013edc5fe184825bb633a8dc862feac35a613d
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-18 05:46:10 +0100

    added new colors

--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -4,25 +4,37 @@ class ThemeBasedAppColors {
   static final Map<String, Color> _lightThemeColors = {
     'primary': const Color(0xFF654321),
     'secondary': const Color(0xFFFEDCBA),
-    'buttonColor': Colors.blueAccent,
+    'buttonColor': Colors.blueGrey,
+    'listTileColor': Colors.blueGrey,
     'textColor': Colors.white,
+    'appbarColor': Colors.grey[100] ?? Colors.white,
+    'selectedColor': Colors.white,
+    'errorColor': Colors.red,
+    'snackBarColor': Colors.white,
+    'dateColor': Colors.white,
+    'listparticipatedColor': Colors.green,
+    'listnotparticipatedColor': Colors.white,
   };
 
   static final Map<String, Color> _darkThemeColors = {
     'primary': const Color(0xFF123456),
     'secondary': const Color(0xFFABCDEF),
     'buttonColor': Colors.tealAccent,
+    'listTileColor': Colors.white,
     'textColor': Colors.grey[900] ?? Colors.black,
-    'backgroundColor': Colors.grey[900] ?? Colors.black,
+    'appbarColor': Colors.grey[900] ?? Colors.black,
+    'selectedColor': Colors.grey[900] ?? Colors.black,
+    'errorColor': Colors.red,
+    'snackBarColor': Colors.grey[900] ?? Colors.black,
+    'dateColor': Colors.grey[900] ?? Colors.black,
+    'listparticipatedColor': Colors.green,
+    'listnotparticipatedColor': Colors.white,
   };
 
   static Color getColor(BuildContext context, String colorKey) {
-    if (Theme.of(context).brightness == Brightness.dark) {
-      return _darkThemeColors[colorKey] ??
-          Colors.black; // Default to black if colorKey not found
-    } else {
-      return _lightThemeColors[colorKey] ??
-          Colors.white; // Default to white if colorKey not found
-    }
+    var themeColors = Theme.of(context).brightness == Brightness.dark
+        ? _darkThemeColors
+        : _lightThemeColors;
+    return themeColors[colorKey] ?? Colors.blue;
   }
 }
