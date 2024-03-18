ab1c9dbfb1635e5568750a150ffdcd9993065fd8
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-18 05:41:44 +0100

    removed

--- lib/appointments/participants/step4_participate_appointment.dart
+++ lib/appointments/participants/step4_participate_appointment.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:survey_app_ks/appointments/appointment_data.dart';
 import 'package:survey_app_ks/appointments/firebase/appointment_services.dart';
+import 'package:survey_app_ks/appointments/participants/user_profile_short.dart';
 import 'package:survey_app_ks/settings/font_size_provider.dart';
+import 'package:survey_app_ks/utilities/colors.dart';
 import 'package:survey_app_ks/utilities/tablet_size.dart';
 
 class TimeSlotParticipantsPage extends StatefulWidget {
@@ -29,6 +31,7 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
   bool _isAdmin = false;
   bool _anyTimeSlotConfirmed = false;
   bool _isLoading = true;
+  String? imageUrl;
 
   late AppointmentService _appointmentService;
 
@@ -51,16 +54,27 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
   void _initPage() async {
     final companyId = await _appointmentService.getCompanyId();
     final isAdmin = await _appointmentService.fetchAdminStatus();
+    List<AppointmentParticipants> allParticipantsWithImages = [];
+
     final allParticipants = await _appointmentService.fetchParticipants(
         widget.appointment.appointmentId, widget.timeSlot);
+
+    for (var participant in allParticipants) {
+      String imageUrl =
+          await _appointmentService.fetchProfileImage(participant.userId);
+      String userName = await _appointmentService.fetchUserName();
+      participant.profileImageUrl = imageUrl;
+      participant.userName = userName;
+      allParticipantsWithImages.add(participant);
+    }
 
     if (!mounted) return;
 
     setState(() {
       this.companyId = companyId;
       _isAdmin = isAdmin;
-      this.allParticipants = allParticipants;
-      filteredParticipants = allParticipants;
+      this.allParticipants = allParticipantsWithImages;
+      filteredParticipants = allParticipantsWithImages;
       _isLoading = false;
     });
   }
@@ -104,17 +118,12 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(right: 40.0, bottom: 8.0),
-          child: Column(
-            children: [
-              Text(
-                widget.appointment.title,
-                style: TextStyle(fontSize: timeFontSize),
-              ),
-            ],
-          ),
+        title: Text(
+          widget.appointment.title,
+          style: TextStyle(
+              fontSize: timeFontSize * 1.3, fontWeight: FontWeight.bold),
         ),
+        backgroundColor: ThemeBasedAppColors.getColor(context, 'appbarColor'),
         centerTitle: true,
       ),
       body: _isLoading
@@ -136,21 +145,30 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
             )
           : Column(
               children: [
-                buildConfirmationStatus(),
-                const SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildParticipantStatus(
-                        Icons.group, Colors.blue, 'all', context),
-                    buildParticipantStatus(Icons.check_circle_outline_outlined,
-                        Colors.green, 'joined', context),
-                    buildParticipantStatus(Icons.help_outline_outlined,
-                        Colors.amber, 'maybe', context),
-                    buildParticipantStatus(
-                        Icons.cancel_outlined, Colors.red, 'declined', context),
-                  ],
+                Card(
+                  child: Column(
+                    children: [
+                      buildConfirmationStatus(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          buildParticipantStatus(
+                              Icons.group, Colors.blue, 'all', context),
+                          buildParticipantStatus(
+                              Icons.check_circle_outline_outlined,
+                              Colors.green,
+                              'joined',
+                              context),
+                          buildParticipantStatus(Icons.help_outline_outlined,
+                              Colors.amber, 'maybe', context),
+                          buildParticipantStatus(Icons.cancel_outlined,
+                              Colors.red, 'declined', context),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
+                const SizedBox(height: 16.0),
                 buildParticipantsList(),
               ],
             ),
@@ -158,15 +176,15 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
   }
 
   Widget buildConfirmationStatus() {
+    final textColor = ThemeBasedAppColors.getColor(context, "textColor");
+    final buttonColor = ThemeBasedAppColors.getColor(context, "buttonColor");
     final fontSize = Provider.of<FontSizeProvider>(context).fontSize;
     final timeFontSize = getTimeFontSize(context, fontSize);
     final timeSlot = widget.timeSlot;
-    final dayOfWeek = DateFormat('EEEE').format(timeSlot.start);
-    final dayOfMonth = DateFormat('d').format(timeSlot.start);
-    final monthOfYear = DateFormat('MMMM').format(timeSlot.start);
     final timeFormat = DateFormat.jm();
     final startTimeString = timeFormat.format(timeSlot.start);
     final endTimeString = timeFormat.format(timeSlot.end);
+    String formattedDate = DateFormat.yMMMd().format(timeSlot.start);
     List<TimeSlot> confirmedTimeSlots = Provider.of<List<TimeSlot>>(context);
 
     bool isTimeSlotConfirmed = confirmedTimeSlots.any((ts) =>
@@ -176,21 +194,73 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
 
     return Column(children: [
       const SizedBox(height: 16.0),
-      Text(
-        '$dayOfWeek $dayOfMonth $monthOfYear',
-        style: TextStyle(fontSize: timeFontSize, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      SizedBox(
+        width: 350,
+        child: Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(
+              color: ThemeBasedAppColors.getColor(context, 'buttonColor'),
+              width: 1,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.calendar_today,
+                      size: timeFontSize * 1.1,
+                      color: buttonColor,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      formattedDate,
+                      style: TextStyle(
+                          fontSize: timeFontSize * 1.1,
+                          fontWeight: FontWeight.bold,
+                          color: buttonColor),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8.0),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      WidgetSpan(
+                        child: Icon(
+                          Icons.access_time,
+                          size: timeFontSize * 1.1,
+                          color: buttonColor,
+                        ),
+                      ),
+                      TextSpan(
+                          text: " $startTimeString - $endTimeString",
+                          style: TextStyle(
+                              fontSize: timeFontSize * 0.9,
+                              fontWeight: FontWeight.bold,
+                              color: buttonColor)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      const SizedBox(height: 8.0),
-      Text('$startTimeString - $endTimeString',
-          style: TextStyle(fontSize: timeFontSize - 2)),
       const SizedBox(height: 16.0),
       if (_isAdmin && !disableButton)
         ElevatedButton(
           style: ElevatedButton.styleFrom(
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0)),
-            backgroundColor: isTimeSlotConfirmed ? Colors.grey : Colors.blue,
+            backgroundColor: isTimeSlotConfirmed ? Colors.grey : buttonColor,
           ),
           onPressed: isTimeSlotConfirmed
               ? null
@@ -199,9 +269,7 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
                       widget.appointment.appointmentId, widget.timeSlot);
                 },
           child: SizedBox(
-            width: isTimeSlotConfirmed
-                ? timeFontSize * 20
-                : timeFontSize * 9, // Adjust width as needed
+            width: isTimeSlotConfirmed ? timeFontSize * 20 : timeFontSize * 9,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -209,23 +277,29 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
                     isTimeSlotConfirmed
                         ? 'this_time_slot_is_confirmed'.tr()
                         : 'confirm'.tr(),
-                    style: TextStyle(fontSize: timeFontSize)),
-                const SizedBox(width: 8.0),
-                Icon(isTimeSlotConfirmed
-                    ? Icons.check_circle_outline_outlined
-                    : Icons.check_circle_outline),
+                    style: TextStyle(
+                      fontSize: timeFontSize,
+                      color: textColor,
+                    )),
+                const SizedBox(width: 20.0),
+                Icon(
+                  isTimeSlotConfirmed
+                      ? Icons.check_circle_outline_outlined
+                      : Icons.check_circle_outline,
+                  size: timeFontSize * 1.3,
+                  color: textColor,
+                ),
               ],
             ),
           ),
         ),
       if (_isAdmin && disableButton)
         Container(
-          width: timeFontSize * 20,
+          width: timeFontSize * 25,
+          height: timeFontSize * 3,
           padding: const EdgeInsets.all(8.0),
           decoration: BoxDecoration(
-            color: Theme.of(context).brightness == Brightness.light
-                ? Colors.grey[200]
-                : Colors.grey[900],
+            color: buttonColor,
             borderRadius: BorderRadius.circular(8.0),
           ),
           child: Row(
@@ -234,7 +308,7 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
               Expanded(
                 child: Text(
                   'another_time_slot_is_confirmed'.tr(),
-                  style: TextStyle(fontSize: timeFontSize),
+                  style: TextStyle(fontSize: timeFontSize, color: textColor),
                   textAlign: TextAlign.center,
                 ),
               ),
@@ -262,54 +336,84 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
         child: ListView.builder(
           itemCount: filteredParticipants?.length,
           itemBuilder: (context, index) {
-            final participant = filteredParticipants?[index];
+            final participant = filteredParticipants![index];
             return Card(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: CircleAvatar(
-                      child: Text(
-                        participant!.userName[0],
-                        style: TextStyle(fontSize: timeFontSize),
-                      ),
-                    ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              elevation: 4,
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+              child: ListTile(
+                leading: GestureDetector(
+                  onTap: () {
+                    userProfile(context, participant);
+                  },
+                  child: CircleAvatar(
+                    radius: timeFontSize * 1.3,
+                    backgroundImage: participant.profileImageUrl.isNotEmpty
+                        ? NetworkImage(participant.profileImageUrl)
+                        : null,
+                    backgroundColor: Colors.blue,
+                    child: participant.profileImageUrl.isEmpty
+                        ? Text(
+                            participant.userName.isNotEmpty
+                                ? participant.userName[0].toUpperCase()
+                                : '?',
+                            style: TextStyle(
+                                fontSize: timeFontSize * 1.1,
+                                color: Colors.white),
+                          )
+                        : null,
                   ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          participant.userName,
-                          style: TextStyle(
-                              fontSize: timeFontSize,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
+                ),
+                title: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Icon(
-                      participant.status == 'joined'
-                          ? Icons.check_circle_outline_outlined
-                          : participant.status == 'maybe'
-                              ? Icons.help_outline_outlined
-                              : Icons.cancel_outlined,
-                      color: participant.status == 'joined'
-                          ? Colors.green
-                          : participant.status == 'maybe'
-                              ? Colors.amber
-                              : Colors.red,
-                      size: timeFontSize * 1.3,
-                    ),
+                  child: Text(
+                    participant.userName,
+                    style: TextStyle(
+                        fontSize: timeFontSize * 1.2,
+                        fontWeight: FontWeight.bold),
                   ),
-                ],
+                ),
+                trailing: Icon(
+                  _getStatusIcon(participant.status),
+                  color: _getStatusColor(participant.status),
+                  size: timeFontSize * 1.5,
+                ),
               ),
             );
           },
         ),
       );
+    }
+  }
+
+  IconData _getStatusIcon(String status) {
+    switch (status) {
+      case 'joined':
+        return Icons.check_circle_outline;
+      case 'maybe':
+        return Icons.help_outline;
+      case 'declined':
+        return Icons.cancel_outlined;
+      default:
+        return Icons.error_outline;
+    }
+  }
+
+  Color _getStatusColor(String status) {
+    switch (status) {
+      case 'joined':
+        return Colors.green;
+      case 'maybe':
+        return Colors.amber;
+      case 'declined':
+        return Colors.red;
+      default:
+        return Colors.grey;
     }
   }
 
