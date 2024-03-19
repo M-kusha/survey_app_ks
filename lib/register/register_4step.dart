163c684de73c23aed11f2e9b4e30428aa81dd79b
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-19 05:46:33 +0100

    added loading indicator when registering and some functions to show when the email is used

--- lib/register/register_4step.dart
+++ lib/register/register_4step.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:survey_app_ks/register/registered_sucesfully.dart';
 import 'package:survey_app_ks/utilities/colors.dart';
@@ -17,9 +18,45 @@ class Register4step extends StatefulWidget {
 
 class Register4stepState extends State<Register4step> {
   final TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _allCompanies = [];
+  List<Map<String, dynamic>> _filteredCompanies = [];
   String? _selectedCompanyId;
+  bool _isSaving = false;
+  bool _isLoading = true;
+  bool _isSearching = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCompanies();
+    _searchController.addListener(_filterCompanies);
+  }
+
+  void _fetchCompanies() async {
+    final companies = await widget.registerLogic.searchCompanies('');
+    if (!mounted) return;
+    setState(() {
+      _allCompanies = companies;
+      _filteredCompanies = companies;
+      _isLoading = false;
+    });
+  }
+
+  void _filterCompanies() {
+    final query = _searchController.text.toLowerCase();
+    final filtered = _allCompanies.where((company) {
+      final nameLower = company['name'].toLowerCase();
+      return nameLower.contains(query);
+    }).toList();
+
+    setState(() => _filteredCompanies = filtered);
+  }
 
   void _finishRegistration() async {
+    setState(() {
+      _isSaving = true;
+    });
+
     try {
       await widget.registerLogic.registerUser(companyId: _selectedCompanyId);
       if (!context.mounted) return;
@@ -29,9 +66,18 @@ class Register4stepState extends State<Register4step> {
             builder: (context) => const RegistrationSuccessPage()),
         (Route<dynamic> route) => false,
       );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        UIUtils.showSnackBar(context, 'email_already_exists'.tr());
+      } else {
+        UIUtils.showSnackBar(context, 'Registration failed: ${e.message}');
+      }
     } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Registration failed: $e')));
+      UIUtils.showSnackBar(context, 'An unexpected error occurred.');
+    } finally {
+      setState(() {
+        _isSaving = false;
+      });
     }
   }
 
@@ -45,18 +91,23 @@ class Register4stepState extends State<Register4step> {
           centerTitle: true,
           backgroundColor:
               ThemeBasedAppColors.getColor(context, 'appbarColor')),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            _buildSearchBar(),
-            const SizedBox(height: 20),
-            _buildCompanyList(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: _selectedCompanyId != null
+      body: _isSaving
+          ? const Center(
+              child: CustomLoadingWidget(
+              loadingText: 'saving_regisration',
+            ))
+          : Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    _buildCompanyList(),
+                  ],
+                ),
+              ),
+            ),
+      bottomNavigationBar: _selectedCompanyId != null && !_isSaving
           ? buildBottomElevatedButton(
               context: context,
               onPressed: _finishRegistration,
@@ -66,70 +117,132 @@ class Register4stepState extends State<Register4step> {
     );
   }
 
-  Widget _buildSearchBar() => Card(
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Card(
         elevation: 2,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-        child: TextField(
-          controller: _searchController,
-          decoration: InputDecoration(
-            hintText: 'search_company'.tr(),
-            prefixIcon: const Icon(Icons.search),
-            border: InputBorder.none,
-            contentPadding: const EdgeInsets.symmetric(vertical: 15),
-          ),
-          onChanged: (value) => setState(() {}),
-        ),
-      );
-
-  Widget _buildLoadingIndicator() {
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      width: 100,
-      height: 100,
-      child: CircularProgressIndicator(
-        valueColor:
-            AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
-        strokeWidth: 6,
+        child: _isSearching
+            ? Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText: 'search_company'.tr(),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ThemeBasedAppColors.getColor(
+                                context, 'buttonColor'),
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                      ),
+                      onChanged: (value) => setState(() {}),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _searchController.clear();
+                      setState(() {
+                        _isSearching = false;
+                      });
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ],
+              )
+            : InkWell(
+                onTap: () {
+                  setState(() {
+                    _isSearching = true;
+                  });
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Icon(Icons.search, color: Colors.grey),
+                ),
+              ),
       ),
     );
   }
 
-  Widget _buildCompanyList() => Expanded(
-        child: FutureBuilder<List<Map<String, dynamic>>>(
-          future: widget.registerLogic.searchCompanies(_searchController.text),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return _buildLoadingIndicator();
-            }
-            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Center(child: Text('company_not_found'.tr()));
-            }
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final company = snapshot.data![index];
-                return _buildCompanyTile(company);
-              },
-            );
-          },
-        ),
-      );
+  Widget _buildCompanyList() {
+    final itemCount = _filteredCompanies.length;
+    final columnCount = (itemCount / 15).ceil();
 
-  Widget _buildCompanyTile(Map<String, dynamic> company) => Card(
-        elevation: isSelected(company['id']) ? 5 : 1,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: ListTile(
-          leading: const Icon(Icons.business),
-          title: Text(company['name']),
-          trailing: isSelected(company['id'])
-              ? const Icon(
-                  Icons.check,
-                )
-              : null,
-          onTap: () => selectCompany(company['id']),
+    return Expanded(
+      child: _isLoading
+          ? const CustomLoadingWidget(
+              loadingText: 'loading',
+            )
+          : ListView.builder(
+              itemCount: columnCount,
+              itemBuilder: (context, columnIndex) {
+                final start = columnIndex * 15;
+                final end = (columnIndex + 1) * 15;
+                final companies = _filteredCompanies.sublist(
+                    start, end > itemCount ? itemCount : end);
+
+                return Column(
+                  children: [
+                    _buildSearchBar(),
+                    const SizedBox(height: 20),
+                    Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.all(20),
+                      shadowColor:
+                          ThemeBasedAppColors.getColor(context, 'buttonColor'),
+                      child: Column(
+                        children: companies
+                            .map((company) => _buildCompanyTile(company))
+                            .toList(),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+    );
+  }
+
+  Widget _buildCompanyTile(Map<String, dynamic> company) => Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: isSelected(company['id']) ? 5 : 1,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  color: isSelected(company['id'])
+                      ? ThemeBasedAppColors.getColor(context, 'buttonColor')
+                      : Colors.transparent,
+                  width: 2,
+                ),
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.business,
+                  color: ThemeBasedAppColors.getColor(context, 'buttonColor'),
+                ),
+                title: Text(company['name']),
+                trailing: isSelected(company['id'])
+                    ? Icon(
+                        Icons.check,
+                        color: ThemeBasedAppColors.getColor(
+                            context, 'buttonColor'),
+                      )
+                    : null,
+                onTap: () => selectCompany(company['id']),
+              ),
+            ),
+          ],
         ),
       );
 
