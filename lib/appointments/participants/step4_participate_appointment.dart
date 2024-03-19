6c8f4951ec5269561a900cc0a9a14a1600f256fe
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-19 05:44:23 +0100

    re deisgned confirm button

--- lib/appointments/participants/step4_participate_appointment.dart
+++ lib/appointments/participants/step4_participate_appointment.dart
@@ -6,6 +6,7 @@ import 'package:survey_app_ks/appointments/firebase/appointment_services.dart';
 import 'package:survey_app_ks/appointments/participants/user_profile_short.dart';
 import 'package:survey_app_ks/settings/font_size_provider.dart';
 import 'package:survey_app_ks/utilities/colors.dart';
+import 'package:survey_app_ks/utilities/reusable_widgets.dart';
 import 'package:survey_app_ks/utilities/tablet_size.dart';
 
 class TimeSlotParticipantsPage extends StatefulWidget {
@@ -62,7 +63,9 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
     for (var participant in allParticipants) {
       String imageUrl =
           await _appointmentService.fetchProfileImage(participant.userId);
-      String userName = await _appointmentService.fetchUserName();
+
+      String userName =
+          await _appointmentService.fetchUserNameById(participant.userId);
       participant.profileImageUrl = imageUrl;
       participant.userName = userName;
       allParticipantsWithImages.add(participant);
@@ -127,25 +130,15 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
         centerTitle: true,
       ),
       body: _isLoading
-          ? Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const CircularProgressIndicator(),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Loading...',
-                    style: TextStyle(
-                      fontSize: timeFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+          ? const CustomLoadingWidget(
+              loadingText: 'loading',
             )
           : Column(
               children: [
                 Card(
+                  elevation: 3,
+                  shadowColor:
+                      ThemeBasedAppColors.getColor(context, 'buttonColor'),
                   child: Column(
                     children: [
                       buildConfirmationStatus(),
@@ -176,7 +169,7 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
   }
 
   Widget buildConfirmationStatus() {
-    final textColor = ThemeBasedAppColors.getColor(context, "textColor");
+    final textColor = ThemeBasedAppColors.getColor(context, "listTileColor");
     final buttonColor = ThemeBasedAppColors.getColor(context, "buttonColor");
     final fontSize = Provider.of<FontSizeProvider>(context).fontSize;
     final timeFontSize = getTimeFontSize(context, fontSize);
@@ -190,7 +183,6 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
     bool isTimeSlotConfirmed = confirmedTimeSlots.any((ts) =>
         ts.start.isAtSameMomentAs(widget.timeSlot.start) &&
         ts.end.isAtSameMomentAs(widget.timeSlot.end));
-    final disableButton = _anyTimeSlotConfirmed && !isTimeSlotConfirmed;
 
     return Column(children: [
       const SizedBox(height: 16.0),
@@ -207,115 +199,117 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
           ),
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            child: Stack(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Icon(
-                      Icons.calendar_today,
-                      size: timeFontSize * 1.1,
-                      color: buttonColor,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.calendar_today,
+                            size: timeFontSize * 1.1, color: buttonColor),
+                        const SizedBox(width: 5),
+                        Text(formattedDate,
+                            style: TextStyle(
+                                fontSize: timeFontSize * 1.1,
+                                fontWeight: FontWeight.bold,
+                                color: buttonColor)),
+                      ],
                     ),
-                    const SizedBox(width: 5),
-                    Text(
-                      formattedDate,
-                      style: TextStyle(
-                          fontSize: timeFontSize * 1.1,
-                          fontWeight: FontWeight.bold,
-                          color: buttonColor),
+                    const SizedBox(height: 8.0),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                              child: Icon(Icons.access_time,
+                                  size: timeFontSize * 1.1,
+                                  color: buttonColor)),
+                          TextSpan(
+                              text: " $startTimeString - $endTimeString",
+                              style: TextStyle(
+                                  fontSize: timeFontSize * 0.9,
+                                  fontWeight: FontWeight.bold,
+                                  color: buttonColor)),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 8.0),
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      WidgetSpan(
+                if (_isAdmin) // Show the icon if the user is an admin
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: !_anyTimeSlotConfirmed || isTimeSlotConfirmed
+                            ? () {
+                                if (!isTimeSlotConfirmed) {
+                                  confirmTimeSlotDialog();
+                                }
+                              }
+                            : null,
                         child: Icon(
-                          Icons.access_time,
-                          size: timeFontSize * 1.1,
-                          color: buttonColor,
+                          isTimeSlotConfirmed ? Icons.alarm_on : Icons.alarm,
+                          color: isTimeSlotConfirmed
+                              ? buttonColor
+                              : _anyTimeSlotConfirmed
+                                  ? Colors.red
+                                  : textColor,
+                          size: timeFontSize * 1.8,
                         ),
                       ),
-                      TextSpan(
-                          text: " $startTimeString - $endTimeString",
-                          style: TextStyle(
-                              fontSize: timeFontSize * 0.9,
-                              fontWeight: FontWeight.bold,
-                              color: buttonColor)),
-                    ],
+                    ),
                   ),
-                ),
               ],
             ),
           ),
         ),
       ),
       const SizedBox(height: 16.0),
-      if (_isAdmin && !disableButton)
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            backgroundColor: isTimeSlotConfirmed ? Colors.grey : buttonColor,
-          ),
-          onPressed: isTimeSlotConfirmed
-              ? null
-              : () async {
-                  await _appointmentService.confirmTimeSlot(
-                      widget.appointment.appointmentId, widget.timeSlot);
-                },
-          child: SizedBox(
-            width: isTimeSlotConfirmed ? timeFontSize * 20 : timeFontSize * 9,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    isTimeSlotConfirmed
-                        ? 'this_time_slot_is_confirmed'.tr()
-                        : 'confirm'.tr(),
-                    style: TextStyle(
-                      fontSize: timeFontSize,
-                      color: textColor,
-                    )),
-                const SizedBox(width: 20.0),
-                Icon(
-                  isTimeSlotConfirmed
-                      ? Icons.check_circle_outline_outlined
-                      : Icons.check_circle_outline,
-                  size: timeFontSize * 1.3,
-                  color: textColor,
-                ),
-              ],
-            ),
-          ),
-        ),
-      if (_isAdmin && disableButton)
-        Container(
-          width: timeFontSize * 25,
-          height: timeFontSize * 3,
-          padding: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            color: buttonColor,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Row(
+    ]);
+  }
+
+  Future<dynamic> confirmTimeSlotDialog() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                child: Text(
-                  'another_time_slot_is_confirmed'.tr(),
-                  style: TextStyle(fontSize: timeFontSize, color: textColor),
-                  textAlign: TextAlign.center,
-                ),
+              Text(
+                "confirm_time_slot".tr(),
+                textAlign: TextAlign.center,
               ),
+              const SizedBox(width: 10),
+              const Icon(Icons.alarm, color: Colors.blueGrey),
             ],
           ),
-        ),
-    ]);
+          content: Text(
+            "confirm_time_slot_tip".tr(),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("cancel".tr()),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("confirm".tr()),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await _appointmentService.confirmTimeSlot(
+                    widget.appointment.appointmentId, widget.timeSlot);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Widget buildParticipantsList() {
@@ -341,7 +335,8 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12.0),
               ),
-              elevation: 4,
+              elevation: 3,
+              shadowColor: ThemeBasedAppColors.getColor(context, 'buttonColor'),
               margin:
                   const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
               child: ListTile(
@@ -354,7 +349,7 @@ class TimeSlotParticipantsPageState extends State<TimeSlotParticipantsPage> {
                     backgroundImage: participant.profileImageUrl.isNotEmpty
                         ? NetworkImage(participant.profileImageUrl)
                         : null,
-                    backgroundColor: Colors.blue,
+                    backgroundColor: Colors.blueGrey,
                     child: participant.profileImageUrl.isEmpty
                         ? Text(
                             participant.userName.isNotEmpty
