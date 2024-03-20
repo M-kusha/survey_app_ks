123ea2c01e035d6f1f2083a9cf0ef0d859afd379
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-20 01:51:40 +0100

    updated settings page to include new UI and functions

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,16 +1,19 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:survey_app_ks/login/login.dart';
 import 'package:survey_app_ks/settings/daten_schutz.dart';
 import 'package:survey_app_ks/settings/email_options.dart';
 import 'package:survey_app_ks/settings/font_size_options.dart';
-import 'package:survey_app_ks/settings/font_size_provider.dart';
 import 'package:survey_app_ks/settings/language_options.dart';
 import 'package:survey_app_ks/settings/notifications_options.dart';
+import 'package:survey_app_ks/settings/password_change.dart';
+import 'package:survey_app_ks/settings/profile_section.dart';
 import 'package:survey_app_ks/settings/text_to_speach_options.dart';
 import 'package:survey_app_ks/settings/theme_options.dart';
+import 'package:survey_app_ks/utilities/colors.dart';
+import 'package:survey_app_ks/utilities/reusable_widgets.dart';
 import 'package:survey_app_ks/utilities/settings_controller.dart';
 import 'package:survey_app_ks/utilities/text_style.dart';
 
@@ -26,10 +29,14 @@ class SettingsPageUI extends StatefulWidget {
 
 class _SettingsPageUIState extends State<SettingsPageUI> {
   double currentFontSize = fontMediumSize;
+  String userID = '';
+
   @override
   void initState() {
     super.initState();
     SettingsController settingsController = SettingsController();
+    _initPage();
+
     settingsController.getFontSize().then((value) {
       setState(() {
         EasyLocalization.of(context)!.setLocale(context.locale);
@@ -37,85 +44,139 @@ class _SettingsPageUIState extends State<SettingsPageUI> {
     });
   }
 
+  void _initPage() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      final userId = currentUser.uid;
+
+      if (userId.isNotEmpty) {
+        setState(() {
+          userID = userId;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Builder(
       builder: (context) {
-        final fontSize = Provider.of<FontSizeProvider>(context).fontSize;
         return Scaffold(
           appBar: AppBar(
             automaticallyImplyLeading: false,
             centerTitle: true,
-            title: Text(
-              'settings'.tr(),
-            ),
+            title: Text('settings'.tr()),
           ),
           body: Container(
             padding: const EdgeInsets.all(10),
             child: ListView(
               children: [
-                NotificationsOptions(
-                  icon: Icons.notifications,
-                  title: 'global_notifications'.tr(),
-                ),
-                dividerSettings,
-                EmailOptions(
-                  icon: Icons.email,
-                  title: 'email_notifications'.tr(),
-                ),
-                dividerSettings,
-                TTSOptions(
-                  icon: Icons.settings_voice,
-                  title: 'text_to_speech'.tr(),
-                ),
-                dividerSettings,
-                ThemeOptionsWidget(
-                    icon: Icons.color_lens, title: 'dark_mode'.tr()),
-                dividerSettings,
-                LanguageOptionsWidget(
-                    icon: Icons.translate_outlined, title: 'language'.tr()),
-                dividerSettings,
-                SizeOptions(
-                  icon: Icons.text_fields,
-                  title: 'font_size'.tr(),
-                ),
-                dividerSettings,
-                DatenSchutzOptionsWidget(
-                  icon: Icons.security,
-                  title: 'datenschutz'.tr(),
-                ),
-                dividerSettings,
-                const SizedBox(height: 30),
-                Center(
-                  child: OutlinedButton(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(horizontal: 100)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: const BorderSide(color: Colors.red),
+                Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  shadowColor:
+                      ThemeBasedAppColors.getColor(context, 'buttonColor'),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: ProfileSection(
+                            userId: userID,
+                          ),
                         ),
                       ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginPage(
-                                  message: '',
-                                )),
-                      );
-                    },
-                    child: Text('log_out'.tr(),
-                        style: TextStyle(fontSize: fontSize)),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: const Icon(Icons.settings),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const PasswordChanger()),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                Card(
+                  shadowColor:
+                      ThemeBasedAppColors.getColor(context, 'buttonColor'),
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    children: [
+                      NotificationsOptions(
+                        icon: Icons.notifications,
+                        title: 'global_notifications'.tr(),
+                      ),
+                      sizedBoxSettings,
+                      EmailOptions(
+                        icon: Icons.email,
+                        title: 'email_notifications'.tr(),
+                      ),
+                      sizedBoxSettings,
+                      TTSOptions(
+                        icon: Icons.settings_voice,
+                        title: 'text_to_speech'.tr(),
+                      ),
+                      ThemeOptionsWidget(
+                        icon: Icons.color_lens,
+                        title: 'dark_mode'.tr(),
+                      ),
+                      sizedBoxSettings,
+                    ],
+                  ),
+                ),
+                Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  shadowColor:
+                      ThemeBasedAppColors.getColor(context, 'buttonColor'),
+                  child: Column(
+                    children: [
+                      DatenSchutzOptionsWidget(
+                        icon: Icons.security,
+                        title: 'datenschutz'.tr(),
+                      ),
+                      sizedBoxSettings,
+                      LanguageOptionsWidget(
+                        icon: Icons.translate_outlined,
+                        title: 'language'.tr(),
+                      ),
+                      sizedBoxSettings,
+                      SizeOptions(
+                        icon: Icons.text_fields,
+                        title: 'font_size'.tr(),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
               ],
             ),
           ),
+          bottomNavigationBar: buildBottomElevatedButton(
+            context: context,
+            onPressed: _onNextPressed,
+            buttonText: 'log_out',
+          ),
         );
       },
+    );
+  }
+
+  void _onNextPressed() async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LoginPage(
+          message: '',
+        ),
+      ),
     );
   }
 }
