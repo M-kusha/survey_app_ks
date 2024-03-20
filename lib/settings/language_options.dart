e873b4d56a15b1a519193ba769747a7505040a14
Kushtrim Mulliqi <mulliqi13@live.com>
2024-03-20 01:50:36 +0100

    added new language

--- lib/settings/language_options.dart
+++ lib/settings/language_options.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
 import 'package:survey_app_ks/settings/font_size_provider.dart';
-import 'package:survey_app_ks/utilities/settings_controller.dart';
+import 'package:survey_app_ks/utilities/text_style.dart';
 
 class LanguageOptionsWidget extends StatefulWidget {
   final IconData icon;
@@ -19,39 +19,38 @@ class LanguageOptionsWidget extends StatefulWidget {
 }
 
 class LanguageOptionsWidgetState extends State<LanguageOptionsWidget> {
-  bool _isExpanded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    loadFontSize();
-  }
+  bool _isLanguageExpanded = false;
+  final Map<String, String> _languageNames = {
+    'en': 'English',
+    'de': 'Deutsch',
+    'sq': 'Shqip',
+  };
 
   void _changeLanguage(BuildContext context, Locale locale) {
-    EasyLocalization.of(context)!.setLocale(locale);
+    context.setLocale(locale);
   }
 
-  void loadFontSize() async {
-    final settingsController = SettingsController();
-    final fontSize = await settingsController.getFontSize();
-
-    // ignore: use_build_context_synchronously
-    Provider.of<FontSizeProvider>(context, listen: false).setFontSize(fontSize);
+  String _getLanguageFullName(Locale locale) {
+    return _languageNames[locale.languageCode] ??
+        locale.languageCode.toUpperCase();
   }
 
   @override
   Widget build(BuildContext context) {
-    final fontSize = Provider.of<FontSizeProvider>(context).fontSize;
+    final fontSizeProvider = Provider.of<FontSizeProvider>(context);
     final currentLocale = context.locale;
+    final List<Locale> locales = context.supportedLocales;
+    Color buttonColor = getButtonColor(context);
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           GestureDetector(
             onTap: () {
               setState(() {
-                _isExpanded = !_isExpanded;
+                _isLanguageExpanded = !_isLanguageExpanded;
               });
             },
             child: Row(
@@ -59,116 +58,53 @@ class LanguageOptionsWidgetState extends State<LanguageOptionsWidget> {
               children: [
                 Row(
                   children: [
-                    Icon(widget.icon, size: fontSize + 15),
-                    const SizedBox(
-                      width: 10,
-                    ),
+                    Icon(widget.icon, size: fontSizeProvider.fontSize + 15),
+                    const SizedBox(width: 10),
                     Text(
                       widget.title,
                       style: TextStyle(
-                          fontSize: fontSize, fontWeight: FontWeight.bold),
+                        fontSize: fontSizeProvider.fontSize,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
-                Icon(Icons.arrow_drop_down, size: fontSize + 15),
+                Icon(
+                  _isLanguageExpanded
+                      ? Icons.arrow_drop_up
+                      : Icons.arrow_drop_down,
+                  size: fontSizeProvider.fontSize + 15,
+                ),
               ],
             ),
           ),
-          if (_isExpanded)
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 1,
-                    blurRadius: 1,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              margin: const EdgeInsets.only(top: 10),
-              height: 100,
-              child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                itemCount:
-                    EasyLocalization.of(context)!.supportedLocales.length,
-                itemBuilder: (context, index) {
-                  final locale =
-                      EasyLocalization.of(context)!.supportedLocales[index];
-
-                  return GestureDetector(
-                    onTap: () => _changeLanguage(context, locale),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Colors.grey.withOpacity(0.5),
-                            width: 0.5,
-                          ),
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          const SizedBox(
-                            height: 40,
-                            width: 20,
-                          ),
-                          Text(
+          const SizedBox(height: 10),
+          if (_isLanguageExpanded)
+            Column(
+              children: locales
+                  .map((locale) => ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: buttonColor,
+                          child: Text(
                             locale.languageCode.toUpperCase(),
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: fontSize,
-                              color: locale == currentLocale
-                                  ? Colors.blue
-                                  : Colors.grey,
+                              fontSize: fontSizeProvider.fontSize - 4,
+                              color: Colors.white,
                             ),
                           ),
-                          const SizedBox(width: 5),
-                          Text(
-                            locale.countryCode?.toUpperCase() ?? '',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: fontSize,
-                              color: locale == currentLocale
-                                  ? Colors.blue
-                                  : Colors.grey,
-                            ),
-                          ),
-                          const Spacer(),
-                          if (locale == currentLocale)
-                            Container(
-                              height: 20,
-                              width: 20,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.blue,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.blue.withOpacity(0.4),
-                                    spreadRadius: 1,
-                                    blurRadius: 1,
-                                    offset: const Offset(0, 5),
-                                  ),
-                                ],
-                              ),
-                              child: Icon(
-                                Icons.check,
-                                color: Colors.white,
-                                size: fontSize,
-                              ),
-                            ),
-                          const SizedBox(
-                            height: 40,
-                            width: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
+                        ),
+                        title: Text(
+                          _getLanguageFullName(locale),
+                          style: TextStyle(fontSize: fontSizeProvider.fontSize),
+                        ),
+                        trailing: locale == currentLocale
+                            ? Icon(Icons.check_circle,
+                                color: buttonColor,
+                                size: fontSizeProvider.fontSize + 10)
+                            : null,
+                        onTap: () => _changeLanguage(context, locale),
+                      ))
+                  .toList(),
             ),
         ],
       ),
